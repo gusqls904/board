f97feb37e2541078065ebc8492f21f399e910a1b
gusql <gusql@DESKTOP-BTAMH27>
2020-11-03 20:58:23 +0900

    게시판 수정까지 완료

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.board.domain.BoardVO;
 import com.board.service.BoardService;
@@ -15,16 +16,55 @@ import com.board.service.BoardService;
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
-	
-	@Inject 
+
+	@Inject
 	BoardService service;
 
-	@RequestMapping(value = "/list" , method=RequestMethod.GET)
-	public void getlist(Model model) throws Exception{
-		
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public void getlist(Model model) throws Exception {
+
 		List<BoardVO> list = service.list();
-				
-		model.addAttribute("list",list);
+
+		model.addAttribute("list", list);
 	}
-	
+
+	// 게시물 작성
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public void getWrite() throws Exception {
+	}
+
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String postWirte(BoardVO vo) throws Exception {
+		service.write(vo);
+
+		return "redirect:/board/list";
+
+	}
+
+	// 게시물 조회
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public void getView(@RequestParam("bno") int bno, Model model) throws Exception {
+
+		BoardVO vo = service.view(bno);
+
+		model.addAttribute("view", vo);
+	}
+
+	// 게시물 수정
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public void getModify(@RequestParam("bno") int bno, Model model) throws Exception {
+		BoardVO vo = service.view(bno);
+
+		model.addAttribute("view", vo);
+	}
+
+	// 게시물 수정
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String postModify(BoardVO vo) throws Exception {
+
+		service.modify(vo);
+
+		return "redirect:/board/view?bno=" + vo.getBno();
+	}
+
 }
